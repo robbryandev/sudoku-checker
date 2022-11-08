425117b4e86af518ad9e03cb9c5f5fd41cfa18e5
robbryandev <robbryandev@gmail.com>
2022-11-08 11:42:40 -0800

    Add Board class constructor

--- src/board.js
+++ src/board.js
@@ -1,1 +1,13 @@
-export class Board {}
+import Tile from "./tile.js";
+
+export default class Board {
+  constructor() {
+    let tileArray = [];
+    for (let i = 0; i < 9; i++) {
+      let newTile = new Tile();
+      tileArray.push(newTile);
+    }
+
+    this.tiles = tileArray;
+  }
+}
